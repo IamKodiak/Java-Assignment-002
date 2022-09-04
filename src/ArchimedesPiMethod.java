e37bd67ad622e26d903c8f794d035e4ee3a59007
Kodiak Drake <kodiakdrake@gmail.com>
2022-09-04 16:12:59 -0700

    Kodiak Drake 9/4/22
    Created a series of variables and equations that form a program to calculate an estimate for PI.

--- src/ArchimedesPiMethod.java
+++ src/ArchimedesPiMethod.java
@@ -1,7 +1,21 @@
+import java.security.KeyStore;
 import java.util.Scanner;
 
 public class ArchimedesPiMethod {
     public static void main(String[] args) {
-
+        while(true) {
+            System.out.println("Please type the number of sides.");
+            Scanner sc = new Scanner(System.in);
+            int n = sc.nextInt();
+            if (n < 1) {
+                break;
+            }
+            double b = 360.0 / n;
+            double a = b / 2;
+            double s = 2 * Math.sin(Math.toRadians(a));
+            double p = s * n;
+            double pi = p / 2;
+            System.out.printf("Our PI estimate is: %f%n%n",pi);
+        }
     }
 }
